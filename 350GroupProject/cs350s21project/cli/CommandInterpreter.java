318d6265fe4cb06cdd6e19d8247eaf0c198d588c
SeanZahller <65538223+SeanZahller@users.noreply.github.com>
2021-05-19 22:18:12 -0700

    Jar working, No constructor

--- 350GroupProject/cs350s21project/cli/CommandInterpreter.java
+++ 350GroupProject/cs350s21project/cli/CommandInterpreter.java
@@ -3,14 +3,9 @@ package cs350s21project.cli;
 public class CommandInterpreter 
 {
 
-	CommandInterpreter()
-	{
-		evaluate("Hello World");
-	}
-	
 	public void evaluate(String commandText)
 	{
-		System.out.println(commandText);
+		
 	}
 	
 	
