7cb58fba90f2822cf4bc7a0466f85920386de413
SeanZahller <65538223+SeanZahller@users.noreply.github.com>
2021-05-31 01:42:33 -0700

    Commands 1.4, 2.8, 3.3 finished. Commented out program crashing code.

--- 350GroupProject/cs350s21project/cli/CommandInterpreter.java
+++ 350GroupProject/cs350s21project/cli/CommandInterpreter.java
@@ -3,6 +3,13 @@ package cs350s21project.cli;
 
 //imports from javadoc
 import cs350s21project.controller.CommandManagers;
+import cs350s21project.controller.command.*;
+import cs350s21project.controller.command.actor.CommandActorCreateActor;
+import cs350s21project.controller.command.actor.CommandActorSetAltitudeDepth;
+import cs350s21project.controller.command.misc.CommandMiscExit;
+import cs350s21project.controller.command.munition.CommandMunitionDefineBomb;
+import cs350s21project.controller.command.munition.CommandMunitionDefineDepthCharge;
+import cs350s21project.controller.command.munition.CommandMunitionDefineMissile;
 import cs350s21project.controller.command.parser.ParseException;
 import cs350s21project.controller.command.view.*;
 import cs350s21project.datatype.*;
@@ -39,6 +46,7 @@ public class CommandInterpreter
 	private Longitude longGridSpacing;
 	private Longitude horizontalExtent;
 	private Longitude longCenter;
+	private AgentID id2;
 	
 	
 	
@@ -67,43 +75,43 @@ public class CommandInterpreter
 		}
 	}//end of evaluate
 	
-	
-	//where we create if statements to match our commands
+
 	private void createCommand(String k) throws ParseException
 	{
 		String command = k.trim();
+		String [] pieces;
 		
 		if (!command.isEmpty() && command != null)
 		{
 			
-			String[] pieces = command.split(" ");
+			pieces = command.split(" ");
 			
-			//Command 1.1 create top window view
-			if(pieces[0].equalsIgnoreCase("create")// if the commands first string = create
+
+			if(pieces[0].equalsIgnoreCase("create"))
 			{
 				
-				if(pieces[1].equalsIgnoreCase("window"))//if the command second string = window
+				if(pieces[1].equalsIgnoreCase("window"))
 				{
-					topViewCommand(pieces, command); // Calling helper method for command 1.1
+					topViewCommand(pieces, command);
 				}
 				
 				
-				else if(pieces[1].equalsIgnoreCase("actor"))//if the command second string = actor
+				else if(pieces[1].equalsIgnoreCase("actor"))
 				{
-					// Calling helper method for command 1.1
+					createActorCommand(pieces, command);
 				}
 				
 				
 				
 			}
 			
-			else if(pieces[0].equalsIgnoreCase("define")// if the commands first string = define
+			else if(pieces[0].equalsIgnoreCase("define"))
 			{
-				if(pieces[1].equalsIgnoreCase("ship"))//if the command second string =ship
+				if(pieces[1].equalsIgnoreCase("ship"))
 				{
 					
 				}
-				else if(pieces[1].equalsIgnoreCase("munition"))//if the command second string = munitions
+				else if(pieces[1].equalsIgnoreCase("munition"))
 				{
 					if(pieces[2].equalsIgnoreCase("bomb"))
 					{
@@ -113,10 +121,14 @@ public class CommandInterpreter
 					{
 						missileCommand(pieces,command);
 					}
+					else if(pieces[2].equalsIgnoreCase("depth_charge"))
+					{
+						depthChargeCommand(pieces,command);
+					}
 					
 					//next munition goes here.
 				}
-				else if(pieces[1].equalsIgnoreCase("sensor"))//if the command second string = sensor
+				else if(pieces[1].equalsIgnoreCase("sensor"))
 				{
 					if(pieces[2].equalsIgnoreCase("radar"))
 					{
@@ -126,7 +138,7 @@ public class CommandInterpreter
 					{
 						thermalCommand(pieces,command);
 					}
-					else if(pieces[2].equalsIgnoreCase("sonar"))//passive
+					else if(pieces[2].equalsIgnoreCase("sonar"))
 					{
 						sonarCommand(pieces,command);
 					}
@@ -139,11 +151,17 @@ public class CommandInterpreter
 			}
 			else if(pieces[0].equalsIgnoreCase("delete"))
 			{
-				
+				if(pieces[1].equalsIgnoreCase("window"))
+				{
+					deleteWindowCommand(pieces, command);
+				}
 			}
 			else if(pieces[0].equalsIgnoreCase("set"))
 			{
-				
+				if(pieces[2].equalsIgnoreCase("altitude|depth"))
+				{
+					setAltitudeCommand(pieces, command);
+				}
 			}
 			else if(pieces[0].equalsIgnoreCase("@load"))
 			{
@@ -168,23 +186,20 @@ public class CommandInterpreter
 			}
 			
 			
-			
-			
-			
+					
 		}
 
 	}
 	
+	
+	
+	
 	// Views
 	private void topViewCommand(String [] pieces, String command) 
 	{
-		this.id = new AgentID(pieces[2]); //create AgentID through cs350s21project.datatype.AgentID from javadoc
-		
-		if(pieces[3].equalsIgnoreCase("top") && pieces[4].equalsIgnoreCase("view")) //If the command matches top view
-		{
-			 this.size = Integer.parseInt(pieces[6]);
-		}
-		
+		this.id = new AgentID(pieces[2]); 
+		this.size = Integer.parseInt(pieces[6]);
+
 		//Test variables to make program work until Latitude/Longitude parse figured out
 		this.center = new Latitude(49, 39, 32);
 		this.extent = new Latitude(0, 10, 0);
@@ -194,30 +209,61 @@ public class CommandInterpreter
 		this.horizontalExtent = new Longitude(0, 10, 0);
 		this.longGridSpacing = new Longitude(0, 0, 30);
 			 
-		//Calling actual command to create top view window, from "use command" in command 1.1 description on pdf
-		//CommandViewCreateWindowTop code is hidden in the jar, found correct parameters in javadoc
-		new CommandViewCreateWindowTop(CommandManagers.getInstance(), command, this.id, this.size, this.center, this.extent,
-            								this.gridSpacing, this.longCenter, this.horizontalExtent, this.longGridSpacing);
-	} // End of command 1.1 helper method
+		cmd.schedule(new CommandViewCreateWindowTop(cmd, command, this.id, this.size, this.center, this.extent,
+            								this.gridSpacing, this.longCenter, this.horizontalExtent, this.longGridSpacing));
+	} 
 	
 	
+	private void deleteWindowCommand(String [] pieces, String command) 
+	{
+		this.id = new AgentID(pieces[2]);
+		
+		cmd.schedule(new CommandViewDeleteWindow(cmd, command, this.id));
+	}
+	
+	
+	
+	//actors
+	private void setAltitudeCommand(String [] pieces, String command) 
+	{
+		this.id = new AgentID(pieces[1]);
+		this.altitude = new Altitude(Double.parseDouble(pieces[2]));
+
+		cmd.schedule(new CommandActorSetAltitudeDepth(cmd, command, this.id, this.altitude));
+	}
+	
+	private void createActorCommand(String [] pieces, String command) 
+	{
+		this.id = new AgentID(pieces[2]);
+		this.id2 = new AgentID(pieces[4]);
+		
+		/* Must parse further and split up lat, long, and alt
+		this.latitude = 
+		this.longitude = 
+		this.altitude = 
+		*/
+		
+		this.coordinates = new CoordinateWorld3D(this.latitude,this.longitude, this.altitude);
+		
+		cmd.schedule(new CommandActorCreateActor(cmd, command, this.id, this.id2, this.coordinates, this.course, this.speed));
+	}
 	
 	
 	
 	//Munitions
 	private void bombCommand(String [] pieces, String command)
 	{
-		this.id = new AgentId(pieces[3]);
+		this.id = new AgentID(pieces[3]);
 		cmd.schedule(new CommandMunitionDefineBomb(cmd,command,id));
 		System.out.print("Created"+ this.id);
 		
 	}
 	private void missileCommand(String [] pieces, String command)
 	{
-		this.id = new AgentId(pieces[3]);
-		this.sensorId = new AgentId(pieces[6]);
-		this.fuzeId = new AgentId(pieces[8]);
-		DistanceNauticalMiles distance = new DistanceNauticalMiles(Double.parseDouble(new AgentId(pieces[11])));
+		this.id = new AgentID(pieces[3]);
+		this.sensorId = new AgentID(pieces[6]);
+		this.fuzeId = new AgentID(pieces[8]);
+		//DistanceNauticalMiles distance = new DistanceNauticalMiles(Double.parseDouble(new AgentID(pieces[11])));
 		
 		
 		cmd.schedule(new CommandMunitionDefineMissile(cmd,command,id,sensorId,fuzeId,distance));
@@ -225,51 +271,59 @@ public class CommandInterpreter
 		
 	}
 	
+	private void depthChargeCommand(String[] pieces, String command)
+	{
+		this.id = new AgentID(pieces[3]);
+		this.id2 = new AgentID(pieces[6]);
+		
+		cmd.schedule(new CommandMunitionDefineDepthCharge(cmd, command, this.id, this.id2));
+	}
+	
 	
 	
 	//sensors
 	private void radarCommand(String [] pieces,String command)
 	{
-		sensorId = new AgentId(pieces[3]);
-		fov =  new AgentId(pieces[8]);
-		power = new AgentId(pieces[10]);
-		sensitivity = new AgentId(pieces[12]);
+		sensorId = new AgentID(pieces[3]);
+		//fov =  new AgentID(pieces[8]);
+		//power = new AgentId(pieces[10]);
+		//sensitivity = new AgentId(pieces[12]);
 		
-		sensitivity = Double.parseDouble(sensitivity);
-		power = Double.parseDouble(power);
-		AngleNavigational angleDegree = new AngleNavigational(Double.parseDouble(fov));
+		//sensitivity = Double.parseDouble(sensitivity);
+		//power = Double.parseDouble(power);
+		//AngleNavigational angleDegree = new AngleNavigational(Double.parseDouble(fov));
 		
-		cmd.schedule(new CommandSensorDefineRadar(cmd,command,sensorId,angleDegree,power, sensitivity));
+		//cmd.schedule(new CommandSensorDefineRadar(cmd,command,sensorId,angleDegree,power, sensitivity));
 		
 	}
 	private void thermalCommand(String[] pieces,String command)
 	{
-		sensorId = new AgentId(pieces[3]);
-		fov =  new AgentId(pieces[8]);
-		sensitivity = new AgentId(pieces[10]);
+		//sensorId = new AgentId(pieces[3]);
+		//fov =  new AgentId(pieces[8]);
+		//sensitivity = new AgentId(pieces[10]);
 		
-		sensitivity = Double.parseDouble(sensitivity);
-		AngleNavigational angleDegree = new AngleNavigational(Double.parseDouble(fov));
+		//sensitivity = Double.parseDouble(sensitivity);
+		//AngleNavigational angleDegree = new AngleNavigational(Double.parseDouble(fov));
 		
-		cmd.schedule(new CommandSensorDefineThermal(cmd,command,sensorId,angleDegreee,sensitivity));
+		//cmd.schedule(new CommandSensorDefineThermal(cmd,command,sensorId,angleDegreee,sensitivity));
 		
 	}
 	private void sonarCommand(String[] pieces,String command)
 	{
-		String type = new AgentId(pieces[3]);
-		sensorId = new AgentId(pieces[4]);
-		if(type.equals("passive"))
+		//String type = new AgentID(pieces[3]);
+		sensorId = new AgentID(pieces[4]);
+		//if(type.equals("passive"))
 		{
-			sensitivity = new AgentId(pieces[7]);
-			sensitivity = Double.parseDouble(sensitivity);
-			cmd.schedule(new CommandSensorDefineSonarPassive(cmd,command,sensorId,sensitivity));
+			//sensitivity = new AgentId(pieces[7]);
+			//sensitivity = Double.parseDouble(sensitivity);
+			//cmd.schedule(new CommandSensorDefineSonarPassive(cmd,command,sensorId,sensitivity));
 			
 		}
-		else if(type.contentEquals("active"))
+		//else if(type.contentEquals("active"))
 		{
 			
 		}
-		else
+	//	else
 		{
 			System.out.print("error");
 		}
@@ -277,11 +331,14 @@ public class CommandInterpreter
 	
 	
 	
+	
+	
+	
 	//MISC
 	private void exitCommand(String[] pieces, String command)
 	{
-		id = new AgentId(pieces[2]);
-		cmd.schedule( new CommandViewExitWindow(cmd,command,id));
+		id = new AgentID(pieces[2]);
+		cmd.schedule( new CommandMiscExit(cmd,command));
 	}
 	
 	
